b58d3aaa6c2318c2e9be6b7ef509ba85a1abf350
Christian Anderson <ca96187@gmail.com>
2019-08-24 16:36:17 -0600

    simplified code a little

--- collatz-conjecture.js
+++ collatz-conjecture.js
@@ -1,13 +1,15 @@
 
-export const steps = (num) => {
+export const steps = num => {
+  let count = 0
+
   if (num <= 0) {
     throw new Error("Only positive numbers are allowed")
   }
 
-  let count = 0
   while (num !== 1) {
     num % 2 === 0 ? num = num / 2 : num = (num * 3) + 1
     count++
   }
+
   return count
 }
