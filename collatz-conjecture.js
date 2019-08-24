bb2aeebd801c6060d2e1bf9de3411f9514ab03c2
Christian Anderson <ca96187@gmail.com>
2019-08-24 16:10:23 -0600

    completed exercise used all tests and made some notes

--- collatz-conjecture.js
+++ collatz-conjecture.js
@@ -1,8 +1,13 @@
-//
-// This is only a SKELETON file for the 'Collatz Conjecture' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
 
-export const steps = () => {
-  throw new Error("Remove this statement and implement this function");
-};
+export const steps = (num) => {
+  if (num <= 0) {
+    throw new Error("Only positive numbers are allowed")
+  }
+
+  let count = 0
+  while (num !== 1) {
+    num % 2 === 0 ? num = num / 2 : num = (num * 3) + 1
+    count++
+  }
+  return count
+}
